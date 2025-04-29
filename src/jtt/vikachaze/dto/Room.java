e4e35a28e6ca1a2a4811a297dc3bdf28be3c6527
lietotajs <lietotajs@224-21>
2025-04-29 11:11:31 +0300

    Toms Gustavs Zeltins Room dto

---
+++ src/jtt/vikachaze/dto/Room.java
@@ -0,0 +1,44 @@
+package jtt.vikachaze.dto;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import javax.imageio.ImageIO;
+
+public class Room {
+private int id;
+private String title;
+private Blob icon;
+
+public Room(String title) {
+this.title = title;
+}
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public Blob getIcon() {
+	return icon;
+}
+public void setIcon(Blob icon) {
+	this.icon = icon;
+}
+
+public Image asImage() throws SQLException, IOException {
+	InputStream in = icon.getBinaryStream();  
+	BufferedImage image = ImageIO.read(in);
+	return image;
+}
+}
