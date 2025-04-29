b2b12bc6f984cb76f1c092fe59b4ad4827b62c6c
lietotajs <lietotajs@224-19>
2025-04-29 10:52:56 +0300

    Leo Vilcāns: generic dao

---
+++ src/jtt/vikachaze/dao/base/GenericDAO.java
@@ -0,0 +1,13 @@
+package jtt.vikachaze.dao.base;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface GenericDAO<T> {
+	int insert(T value) throws SQLException;
+	int update(T value) throws SQLException;
+	int delete(T value) throws SQLException;
+	int getID(T value) throws SQLException;
+	T getByID(int id) throws SQLException;
+	List<T> getAllData() throws SQLException;
+}
