48b4fe2a3f2f84c2cf5628bcaff9d542025da62c
lietotajs <lietotajs@224-20>
2025-04-29 12:31:30 +0300

    Tomass Kamišovs: PostDAO

---
+++ src/jtt/vikachaze/dao/PostDAO.java
@@ -0,0 +1,17 @@
+package jtt.vikachaze.dao;
+
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+import jtt.vikachaze.dao.base.GenericDAO;
+import jtt.vikachaze.dto.Post;
+import jtt.vikachaze.dto.User;
+
+public interface PostDAO extends GenericDAO<Post>{
+	final String TABLE = "post";
+	
+	Post getPostByTitle(String title) throws SQLException;
+	Post getPostByUser(User user) throws SQLException;
+	Post getPostByText(String text) throws SQLException;
+	Post getPostByTime(Timestamp time) throws SQLException;
+}
