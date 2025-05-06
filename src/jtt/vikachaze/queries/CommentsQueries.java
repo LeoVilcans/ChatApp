5fced95a977bc7667cfa346d4fa350d01d943e2a
lietotajs <lietotajs@224-20>
2025-05-06 11:57:57 +0300

    Tomass Kamišovs: PostLikesQueries, CommentsQueries

---
+++ src/jtt/vikachaze/queries/CommentsQueries.java
@@ -0,0 +1,23 @@
+package jtt.vikachaze.queries;
+
+public interface CommentsQueries {
+	public static final String TABLE = "comments";
+
+    public static final String INSERT_QUERY = "INSERT INTO " + TABLE + " (sent_time, user_id, post_id, text) VALUES (?, ?, ?, ?)";
+    
+    public static final String UPDATE_QUERY = "UPDATE " + TABLE + " SET sent_time = ?, user_id = ?, post_id = ?, text = ? WHERE id = ?";
+
+    public static final String DELETE_QUERY = "DELETE FROM " + TABLE + " WHERE id = ?";
+    
+    public static final String GET_ID_QUERY = "SELECT * FROM " + TABLE + " WHERE sent_time = ?, AND user_id = ? AND post_id = ? AND text = ?";
+    
+    public static final String GET_BY_ID_QUERY = "SELECT * FROM " + TABLE + " WHERE id = ?";
+    
+    public static final String GET_BY_USER_QUERY = "SELECT * FROM " + TABLE + " WHERE user_id = ?";
+    
+    public static final String GET_BY_POST_QUERY = "SELECT * FROM " + TABLE + " WHERE post_id = ?";
+    
+    public static final String GET_BY_TEXT_QUERY = "SELECT * FROM " + TABLE + " WHERE text = ?";
+    
+    public static final String GET_ALL_QUERY = "SELECT * FROM " + TABLE;
+}
---
+++ src/jtt/vikachaze/queries/PostLikesQueries.java
@@ -0,0 +1,19 @@
+package jtt.vikachaze.queries;
+
+public interface PostLikesQueries {
+	public static final String TABLE = "post_likes";
+
+    public static final String INSERT_QUERY = "INSERT INTO " + TABLE + " (user_id, post_id) VALUES (?, ?)";
+
+    public static final String DELETE_QUERY = "DELETE FROM " + TABLE + " WHERE id = ?";
+    
+    public static final String GET_ID_QUERY = "SELECT * FROM " + TABLE + " WHERE user_id = ? AND post_id = ?";
+    
+    public static final String GET_BY_ID_QUERY = "SELECT * FROM " + TABLE + " WHERE id = ?";
+    
+    public static final String GET_BY_USER_QUERY = "SELECT * FROM " + TABLE + " WHERE user_id = ?";
+    
+    public static final String GET_BY_POST_QUERY = "SELECT * FROM " + TABLE + " WHERE post_id = ?";
+    
+    public static final String GET_ALL_QUERY = "SELECT * FROM " + TABLE;
+}
