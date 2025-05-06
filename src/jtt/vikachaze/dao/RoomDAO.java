6e82a6061cb1ef0282df8a991a14173e349adfc6
lietotajs <lietotajs@224-21>
2025-05-06 10:22:01 +0300

    Toms Gustavs Zeltins RoomDAOImpl

--- src/jtt/vikachaze/dao/RoomDAO.java
+++ src/jtt/vikachaze/dao/RoomDAO.java
@@ -1,6 +1,7 @@
 package jtt.vikachaze.dao;
 
 import java.sql.SQLException;
+import java.util.List;
 
 import jtt.vikachaze.dao.base.GenericDAO;
 import jtt.vikachaze.dto.Room;
@@ -8,5 +9,5 @@ import jtt.vikachaze.dto.Room;
 public interface RoomDAO extends GenericDAO<Room>{
 	final String TABLE = "rooms";
 	
-	Room getByTitle(String title) throws SQLException;
+	List<Room> getByTitle(String title) throws SQLException;
 }
--- src/jtt/vikachaze/dao/impl/RoomDAOImpl.java
+++ src/jtt/vikachaze/dao/impl/RoomDAOImpl.java
@@ -5,38 +5,89 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import jtt.vikachaze.connection.Database;
 import jtt.vikachaze.dao.RoomDAO;
 import jtt.vikachaze.dto.Room;
-import jtt.vikachaze.dto.User;
 import jtt.vikachaze.queries.RoomQueries;
 
 public class RoomDAOImpl implements RoomDAO, RoomQueries{
 
 	@Override
 	public int insert(Room value) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection connection = Database.getConnection();
+	    PreparedStatement statement = connection.prepareStatement(INSERT_QUERY);
+	    
+	    List<Room> rooms = getAllData();
+	    
+	    for (Room room : rooms) {
+	    	if (room.getTitle().equals(value.getTitle())) {
+	    		return 0;
+	    	}
+	    }
+	    
+	    statement.setString(1, value.getTitle());
+	    statement.setBlob(2, value.getIcon());
+
+	    int result = statement.executeUpdate();
+
+	    Database.closePreparedStatement(statement);
+	    Database.closeConnection(connection);
+	    return result;
 	}
 
 	@Override
 	public int update(Room value) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection connection = Database.getConnection();
+	    PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY);
+	    
+	    statement.setString(1, value.getTitle());
+	    statement.setBlob(2, value.getIcon());
+	    statement.setInt(3, value.getId());
+
+
+	    int result = statement.executeUpdate();
+
+	    Database.closePreparedStatement(statement);
+	    Database.closeConnection(connection);
+	    return result;
 	}
 
 	@Override
 	public int delete(Room value) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection connection = Database.getConnection();
+	    PreparedStatement statement = connection.prepareStatement(DELETE_QUERY);
+
+	    statement.setInt(1, value.getId());
+
+	    int result = statement.executeUpdate();
+	    
+	    Database.closePreparedStatement(statement);
+	    Database.closeConnection(connection);
+	    return result;
 	}
 
 	@Override
 	public int getID(Room value) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection connection = Database.getConnection();
+	    PreparedStatement statement = connection.prepareStatement(GET_ID_QUERY);
+
+	    statement.setString(1, value.getTitle());
+	    statement.setBlob(2, value.getIcon());
+	    
+	    ResultSet result = statement.executeQuery();
+
+	    int id = 0;
+	    if (result.next()) {
+	        id = result.getInt("id");
+	    }
+
+	    Database.closeResultSet(result);
+	    Database.closePreparedStatement(statement);
+	    Database.closeConnection(connection);
+	    return id;
 	}
 
 	@Override
@@ -53,7 +104,7 @@ public class RoomDAOImpl implements RoomDAO, RoomQueries{
 		String title = result.getString("title");
 		
 		Blob icon = result.getBlob("icon");
-		if (!result.wasNull()) {
+		if (result.wasNull()) {
 			icon = null;
 		}
 		
@@ -70,14 +121,67 @@ public class RoomDAOImpl implements RoomDAO, RoomQueries{
 
 	@Override
 	public List<Room> getAllData() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+Connection connection = Database.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement(GET_ALL_QUERY, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+	
+		ResultSet result = statement.executeQuery();
+		
+		List<Room> rooms = new ArrayList<Room>();
+		while (result.next()) {
+			String title = result.getString("title");
+			
+			
+			Blob icon = result.getBlob("icon");
+			if (result.wasNull()) {
+				icon = null;
+			}
+			
+			Room room = new Room(title);
+			room.setIcon(icon);
+
+			
+			rooms.add(room);
+		}
+		
+		Database.closeResultSet(result);
+		Database.closePreparedStatement(statement);
+		Database.closeConnection(connection);
+		
+		return rooms;
 	}
 
 	@Override
-	public Room getByTitle(String title) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Room> getByTitle(String title) throws SQLException {
+Connection connection = Database.getConnection();
+		
+		PreparedStatement statement = connection.prepareStatement(GET_BY_TITLE, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		statement.setString(1, title);
+		
+		ResultSet result = statement.executeQuery();
+		
+		List<Room> rooms = new ArrayList<Room>();
+		while (result.next()) {
+			
+			
+			
+			Blob icon = result.getBlob("icon");
+			if (result.wasNull()) {
+				icon = null;
+			}
+			
+			Room room = new Room(title);
+			room.setIcon(icon);
+
+			
+			rooms.add(room);
+		}
+		
+		Database.closeResultSet(result);
+		Database.closePreparedStatement(statement);
+		Database.closeConnection(connection);
+		
+		return rooms;
 	}
 
 }
