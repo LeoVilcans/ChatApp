9f23ef490f68ce8be58d1f417a3514d27d317e4c
lietotajs <lietotajs@224-21>
2025-05-12 14:56:19 +0300

    Toms Gustavs Zeltins RegisterGUI

---
+++ src/jtt/vikachaze/gui/RegisterGUI.java
@@ -0,0 +1,164 @@
+package jtt.vikachaze.gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import jtt.vikachaze.connection.Database;
+import jtt.vikachaze.dao.UserDAO;
+import jtt.vikachaze.dto.User;
+import jtt.vikachaze.util.Scalr;
+import jtt.vikachaze.dao.impl.UserDAOImpl;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.ImagingOpException;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
+
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+public class RegisterGUI extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField usernameField;
+	private JPasswordField passwordField;
+	private UserDAO userDAO = new UserDAOImpl();
+	private File currentAttachment = null;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RegisterGUI frame = new RegisterGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public RegisterGUI() {
+		setTitle("Register");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 283, 379);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblRegister = new JLabel("REGISTER");
+		lblRegister.setHorizontalAlignment(SwingConstants.CENTER);
+		lblRegister.setFont(new Font("Tahoma", Font.PLAIN, 23));
+		lblRegister.setBounds(10, 11, 247, 45);
+		contentPane.add(lblRegister);
+		
+		JLabel usernameLabel = new JLabel("Username:");
+		usernameLabel.setBounds(10, 72, 80, 14);
+		contentPane.add(usernameLabel);
+		
+		usernameField = new JTextField();
+		usernameField.setColumns(10);
+		usernameField.setBounds(10, 87, 247, 33);
+		contentPane.add(usernameField);
+		
+		JLabel passwordLabel = new JLabel("Password:");
+		passwordLabel.setBounds(10, 131, 80, 14);
+		contentPane.add(passwordLabel);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(10, 146, 247, 33);
+		contentPane.add(passwordField);
+		
+		JButton registerButton = new JButton("Register");
+		registerButton.setBounds(10, 282, 247, 45);
+		contentPane.add(registerButton);
+		
+		JLabel lblProfilePicture = new JLabel("Profile picture:");
+		lblProfilePicture.setBounds(10, 192, 96, 14);
+		contentPane.add(lblProfilePicture);
+		
+		JButton AddProfilePictureButton = new JButton("Add profile picture");
+		AddProfilePictureButton.setBounds(10, 219, 247, 45);
+		contentPane.add(AddProfilePictureButton);
+		
+registerButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					register();
+					
+					
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+
+			
+		});
+		
+		AddProfilePictureButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(e.getSource()==AddProfilePictureButton) {
+					JFileChooser fileChooser = new JFileChooser();
+					int response = fileChooser.showOpenDialog(null);
+					
+					if(response == JFileChooser.APPROVE_OPTION) {
+						currentAttachment = new File(fileChooser.getSelectedFile().getAbsolutePath());
+						JOptionPane.showMessageDialog(RegisterGUI.this, "Bilde tika pievienota.", getTitle(), JOptionPane.INFORMATION_MESSAGE);
+						
+						}
+				}
+			}
+			
+		});
+		
+	}
+	public void register() throws SQLException {
+		String username = usernameField.getText();
+		char[] password = passwordField.getPassword();
+		String str = String.valueOf(password);
+		
+		User user = new User(username,str);
+		
+		
+		
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try {
+			ImageIO.write(Scalr.resize(ImageIO.read(currentAttachment), 236), "jpg", baos);
+			Blob b1 = Database.getConnection().createBlob();
+			b1.setBytes(1,  baos.toByteArray());
+			user.setPfp(b1);
+		} catch (IllegalArgumentException | ImagingOpException | IOException | SQLException e) {
+			e.printStackTrace();
+		}
+		userDAO.insert(user);
+	}
+}
