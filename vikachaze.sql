34314abee08e5b4721948e5ec8cbb63357753dfb
lietotajs <lietotajs@224-19>
2025-05-20 11:13:01 +0300

    Leo Vilcāns: redigets sql

--- vikachaze.sql
+++ vikachaze.sql
@@ -5,20 +5,20 @@ CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(20) NOT NULL,
     password VARCHAR(255) NOT NULL,
-	status VARCHAR(100),
+	status VARCHAR(300),
 	pfp LONGBLOB
 );
 
 CREATE TABLE rooms (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	title VARCHAR(100) NOT NULL,
+	title VARCHAR(200) NOT NULL,
 	icon LONGBLOB
 );
 
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
 	room_id INT NOT NULL,
-	text VARCHAR(250) NOT NULL,
+	text VARCHAR(2500) NOT NULL,
 	sent_time TIMESTAMP NOT NULL,
     attachment LONGBLOB,
     user_id INT NOT NULL,
@@ -32,8 +32,8 @@ CREATE TABLE posts (
 	sent_time TIMESTAMP NOT NULL,
     attachment LONGBLOB,
     user_id INT NOT NULL,
-	title VARCHAR(100) NOT NULL,
-    text VARCHAR(1000) NOT NULL,
+	title VARCHAR(200) NOT NULL,
+    text VARCHAR(2500) NOT NULL,
     
     CONSTRAINT fk_post_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
@@ -43,7 +43,7 @@ CREATE TABLE comments (
 	sent_time TIMESTAMP NOT NULL,
     user_id INT NOT NULL,
     post_id INT NOT NULL,
-    text VARCHAR(250) NOT NULL,
+    text VARCHAR(1000) NOT NULL,
     
     CONSTRAINT fk_comment_post FOREIGN KEY (post_id) REFERENCES posts(id) ON DELETE CASCADE,
     CONSTRAINT fk_comment_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
