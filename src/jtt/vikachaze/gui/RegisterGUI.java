e1213c38b7684c849770130e8137c0d50d1cb789
lietotajs <lietotajs@224-21>
2025-05-21 12:50:56 +0300

    Zeltins on top

--- src/jtt/vikachaze/gui/RegisterGUI.java
+++ src/jtt/vikachaze/gui/RegisterGUI.java
@@ -126,6 +126,7 @@ public class RegisterGUI extends JFrame {
 		String username = usernameField.getText();
 		char[] password = passwordField.getPassword();
 		String str = String.valueOf(password);
+		str = str+"Zeltins";
 		
 		User user = new User(username,str);
 		
